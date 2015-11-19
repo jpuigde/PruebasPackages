be6f36ea7cc4cf2dcea962e81974b3b817c43fbc
jpuigde <jpuigde@gmail.com>
2015-11-19 16:05:55 +0100

    .

--- svm.R
+++ svm.R
@@ -1,6 +1,8 @@
 require(ggplot2)
 
-n=2000
+n=1000
+
+mislead <- sample(n,n/10)
 
 a = runif(1)
 b = runif(1)
@@ -11,21 +13,32 @@ r=4
 
 datos <- data.frame( x = x , y = y ,
                      x0 = r*cos(seq(0,2*pi,length.out=n))/(a),
-                     y0 = r*sin(seq(0,2*pi,length.out=n))/(b) , z = as.factor(ifelse( a*a*x*x+b*b*y*y < r*r , 1 , 0 ) ))
+                     y0 = r*sin(seq(0,2*pi,length.out=n))/(b) , z = ifelse( a*a*x*x+b*b*y*y < r*r , 1 , 0 ) )
+
+datos$z <- ifelse ( datos$z==1 | datos$x > datos$y , 1 , 0  )
+
+datos$z[mislead] <- 1 - datos$z[mislead]
 
 ggplot()+
-  geom_point(aes(x=datos$x,y=datos$y,shape=datos$z,colour=pred))+
+  geom_point(aes(x=datos$x,y=datos$y,shape=as.factor(datos$z),colour=datos$z))+
   geom_path(aes(x=datos$x0,y=datos$y0))
 
 require(e1071)
-svmModel = svm(x+y~z,data=datos)
+svmModel = svm(x+y~z,data=datos,)
 
+tuner <- tune(svm, x+y~z, data = datos, 
+     ranges = list(gamma = seq(0.1,2,0.1), cost = seq(0.1,2,10)),
+     tunecontrol = tune.control(sampling = "bootstrap")
+)
 
-print(svmModel)
-summary(svmModel)
+tuner$best.parameters
+svmModel = svm(x+y~z,data=datos,gamma=tuner$best.parameters$gamma,cost=tuner$best.parameters$cost)
 
-# test with train data
-pred <- predict(svmModel, datos)
+pred <- predict( svmModel , datos )
+ggplot() +
+  geom_point( aes( x = datos$x  , y = datos$y , shape = as.factor( datos$z ) , colour = pred ) ) +
+  geom_path(  aes( x = datos$x0 , y = datos$y0 ) )
+
 
 
 # (same as:)
